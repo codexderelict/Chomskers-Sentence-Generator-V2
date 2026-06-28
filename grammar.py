541ede47fd6408e603955b44e71abe8024c12311
Omar <magmarashis@gmail.com>
2026-06-28 05:23:12 +0400

    Transitivity added!

--- grammar.py
+++ grammar.py
@@ -1,23 +1,23 @@
 grammar1 = {
     "S": ["NP", "VP"],
     "NP": ["Det", "N"],
-    "VP": ["V"]
+    "VP": ["V_intr"]
 }
 grammar2 = {
     "S": ["NP", "VP"],
     "NP": ["Det", "Adj", "N"],
-    "VP": ["V", "NP"]
+    "VP": ["V_trans", "NP"]
 }
 grammar3 = {
     "S": ["NP", "VP"],
     "NP": ["Det", "Adv", "Adj", "N"],
-    "VP": ["V", "NP", "Adv"]
+    "VP": ["V_trans", "NP", "Adv"]
 }
 grammar4 = {
     "S": ["NP", "VP"],
-    "VP": ["V", "PP"],
+    "VP": ["V_intr", "PP"],
     "NP": ["Det", "Adj", "N"],
     "PP": ["P", "NP"]
 }
-POSTags = ["Det", "N", "V", "Adj", "Adv", "P"]
+POSTags = ["Det", "N", "V_trans", "V_intr", "Adj", "Adv", "P"]
 grammarList = [grammar1, grammar2, grammar3, grammar4]
