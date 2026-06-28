b2f6e787b5e73d39ec10416ce2ff17b1b30286a2
CodexDerelict <codexderelict@proton.me>
2026-06-28 11:59:39 +0400

    I fixed it.

--- grammar.py
+++ grammar.py
@@ -35,9 +35,8 @@ grammar6 = {
 }
 grammar7 = {
     "S": ["NP", "VP"],
-    "VP": ["VP'", "Conj", "NP", "VP'"], 
-    # I know this is not how it works. I haven't slept last night and this is better than either S' or a RecursionError. I know it doesn't conform to how 
-    # English CFGs are written, but it's STILL a valid CFG rule from a formal language theory perspective. A non-terminal on the LHS, a string on the RHS. SO SUCK ON IT! 
+    "S'": ["NP", "VP'"]
+    "VP": ["VP'", "Conj", "S'"], 
     "VP'": ["V_trans", "NP", "PP"],
     "PP": ["P", "NP"],
     "NP":["Det", "Adj", "N"]
