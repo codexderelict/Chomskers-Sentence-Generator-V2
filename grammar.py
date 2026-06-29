ee401c2affd094cb694190da73f17f6f22a84e35
CodexDerelict <codexderelict@proton.me>
2026-06-30 01:04:42 +0400

    Fixed it so grammar1 is the only one that can run to minimize errors

--- grammar.py
+++ grammar.py
@@ -7,4 +7,4 @@ grammar1 = {
 # I deleted all of the grammars to make new ones that are more elegant and fit English CFG descriptions better. 
 
 POSTags = ["Det", "N", "V_trans", "V_intr", "Adj", "Adv", "P", "C_n", "C_v", "Conj"]
-grammarList = [grammar1, grammar2, grammar3, grammar4, grammar5, grammar6, grammar7]
+grammarList = [grammar1]
