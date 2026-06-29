da4fb329a42c15eb9eec7fe7f82ec7766fd2501b
CodexDerelict <codexderelict@proton.me>
2026-06-30 01:01:21 +0400

    Deleted all grammars in order to build new ones that fit transformational grammars better

--- grammar.py
+++ grammar.py
@@ -6,8 +6,5 @@ grammar1 = {
 
 # I deleted all of the grammars to make new ones that are more elegant and fit English CFG descriptions better. 
 
-
-# I fixed it. S' is now a proper non-terminal and it fits an English CFG. I have no clue why I didn't do this earlier. Probably because of sleep deprivation.
-# I need a Monster... 
 POSTags = ["Det", "N", "V_trans", "V_intr", "Adj", "Adv", "P", "C_n", "C_v", "Conj"]
 grammarList = [grammar1, grammar2, grammar3, grammar4, grammar5, grammar6, grammar7]
